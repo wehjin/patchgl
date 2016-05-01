3d7305de65a86a2c5cffd0e18fecc2e1b473d210
Jeffrey Yu <wehjin@gmail.com>
2016-05-01 14:12:31 -0700

    Launch patchgl from elixir

--- shui/lib/shui.ex
+++ shui/lib/shui.ex
@@ -1,2 +1,25 @@
 defmodule Shui do
+
+    def open() do
+        port = Port.open({:spawn, "patchgl"}, [:stream, :binary, :exit_status, :hide, :use_stdio, :stderr_to_stdout])
+        handle_output(port)
+    end
+
+    def handle_output(port) do
+      receive do
+        {:begin_patch, data} ->
+            send(port, {self, {:command, data}})
+            handle_output(port)
+        {^port, {:data, data}} ->
+            IO.puts(data)
+            handle_output(port)
+        :close ->
+            IO.puts("Closing port")
+            Port.close(port)
+        {^port, {:exit_status, status}} ->
+            status
+        {^port, :closed} ->
+            :closed
+      end
+    end
 end
---
+++ shui/lib/shui/cli.ex
@@ -0,0 +1,10 @@
+defmodule Shui.CLI do
+
+    def run() do
+        process()
+    end
+
+    def process do
+        :exit
+    end
+end
