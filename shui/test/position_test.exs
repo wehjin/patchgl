31c876c634090815f4e8bb87f45cd30396a1d14f
Jeffrey Yu <wehjin@gmail.com>
2016-05-05 06:31:13 -0700

    Add test for Position.split_horizontal

---
+++ shui/test/position_test.exs
@@ -0,0 +1,22 @@
+defmodule PositionTest do
+  use ExUnit.Case
+  doctest Shui.Position
+
+  alias Shui.Position
+
+  test "position" do
+    position = Position.position(1,2,3,4)
+    assert position[:left] == 1
+    assert position[:bottom] == 2
+    assert position[:right] == 3
+    assert position[:top] == 4
+  end
+
+  test "split_horizontal" do
+    {l, r} = Position.split_horizontal(Position.position(0, 0, 100, 100), 0.5)
+    assert l[:left] == 0 && r[:left] == 50
+    assert l[:right] == 50 && r[:right] == 100
+    assert l[:bottom] == 0 && r[:bottom] == 0
+    assert l[:top] == 100 && r[:top] == 100
+  end
+end
