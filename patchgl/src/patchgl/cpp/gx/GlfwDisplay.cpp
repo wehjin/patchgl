aa14fa4e2265194c9fd2ab7e588f2fa112ca627b
Jeffrey Yu <wehjin@gmail.com>
2016-06-16 05:41:25 -0700

    Add color to rectangle

--- patchgl/src/patchgl/cpp/gx/GlfwDisplay.cpp
+++ patchgl/src/patchgl/cpp/gx/GlfwDisplay.cpp
@@ -16,17 +16,21 @@ using namespace rxcpp::util;
 using namespace std;
 
 // Shaders
-const GLchar *vertexShaderSource = "#version 330 core\n"
+const GLchar* vertexShaderSource = "#version 330 core\n"
         "layout (location = 0) in vec3 position;\n"
+        "layout (location = 1) in vec3 color;\n"
+        "out vec3 ourColor;\n"
         "void main()\n"
         "{\n"
-        "gl_Position = vec4(position.x, position.y, position.z, 1.0);\n"
+        "gl_Position = vec4(position, 1.0);\n"
+        "ourColor = color;\n"
         "}\0";
-const GLchar *fragmentShaderSource = "#version 330 core\n"
+const GLchar* fragmentShaderSource = "#version 330 core\n"
+        "in vec3 ourColor;\n"
         "out vec4 color;\n"
         "void main()\n"
         "{\n"
-        "color = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+        "color = vec4(ourColor, 1.0f);\n"
         "}\n\0";
 
 void key_callback(GLFWwindow *window, int key, int scancode, int action, int mods) {
@@ -149,12 +153,13 @@ void GlfwDisplay::awaitClose() {
     unsigned int triangles = rectangles * 2;
     unsigned int vertexCount = triangles * 3;
     GLfloat vertices[] = {
-            -0.5f, -0.5f, 0.0f, // Bottom Left
-            0.5f, -0.5f, 0.0f, // Bottom Right
-            0.5f, 0.5f, 0.0f,  // Top Right
-            0.5f, 0.5f, 0.0f,  // Top Right
-            -0.5f, 0.5f, 0.0f,  // Top Left
-            -0.5f, -0.5f, 0.0f, // Bottom Left
+            // Positions        //Colors
+            -0.5f, -0.5f, 0.0f, 1.0f, 0.0f, 0.0f,// Bottom Left
+            0.5f, -0.5f, 0.0f, 1.0f, 0.0f, 0.0f,// Bottom Right
+            0.5f, 0.5f, 0.0f,  0.0f, 1.0f, 0.0f,// Top Right
+            0.5f, 0.5f, 0.0f,  0.0f, 1.0f, 0.0f,// Top Right
+            -0.5f, 0.5f, 0.0f,  0.0f, 1.0f, 0.0f,// Top Left
+            -0.5f, -0.5f, 0.0f, 1.0f, 0.0f, 0.0f,// Bottom Left
     };
     GLuint VBO, VAO;
     glGenVertexArrays(1, &VAO);
@@ -165,8 +170,13 @@ void GlfwDisplay::awaitClose() {
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (GLvoid *) 0);
+    // Position attribute
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), (GLvoid *) 0);
     glEnableVertexAttribArray(0);
+
+    // Color attribute
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), (GLvoid *) (3 * sizeof(GLfloat)));
+    glEnableVertexAttribArray(1);
 
     glBindBuffer(GL_ARRAY_BUFFER, 0);
     glBindVertexArray(0);
