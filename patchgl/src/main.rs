fef4d00a06aa181dea3c4635839413e8a0558a91
Jeffrey Yu <wehjin@gmail.com>
2016-11-05 12:49:57 -0700

    Draw rectangle patch with triangle strip

--- patchgl/src/main.rs
+++ patchgl/src/main.rs
@@ -1,52 +1,80 @@
 #[macro_use] extern crate glium;
 
-fn main() {
-    use glium::{DisplayBuild, Surface};
-    let display = glium::glutin::WindowBuilder::new().build_glium().unwrap();
+#[derive(Copy, Clone)]
+struct Vertex {
+    position: [f32; 2],
+}
+implement_vertex!(Vertex, position);
 
-    #[derive(Copy, Clone)]
-    struct Vertex {
-        position: [f32; 2],
+struct PatchRenderer {
+    display: glium::backend::glutin_backend::GlutinFacade,
+    program: glium::Program,
+}
+
+impl PatchRenderer {
+    fn new() -> Self {
+        use glium::{DisplayBuild};
+        let display = glium::glutin::WindowBuilder::new().build_glium().unwrap();
+
+        let vertex_shader_src = r#"
+            #version 140
+            in vec2 position;
+            void main() {
+                gl_Position = vec4(position, 0.0, 1.0);
+            }
+        "#;
+
+        let fragment_shader_src = r#"
+            #version 140
+            out vec4 color;
+            void main() {
+                color = vec4(1.0, 0.0, 0.0, 1.0);
+            }
+        "#;
+
+        let program = glium::Program::from_source(&display, vertex_shader_src, fragment_shader_src, None).unwrap();
+        PatchRenderer { display: display, program: program }
     }
-    implement_vertex!(Vertex, position);
+}
 
-    let vertex1 = Vertex { position: [-0.5, -0.5] };
-    let vertex2 = Vertex { position: [0.0, 0.5] };
-    let vertex3 = Vertex { position: [0.5, -0.25] };
-    let shape = vec![vertex1, vertex2, vertex3];
 
-    let vertex_buffer = glium::VertexBuffer::new(&display, &shape).unwrap();
-    let indices = glium::index::NoIndices(glium::index::PrimitiveType::TrianglesList);
-    let vertex_shader_src = r#"
-		#version 140
-		in vec2 position;
-		void main() {
-			gl_Position = vec4(position, 0.0, 1.0);
-		}
-	"#;
+fn main() {
+    use glium::{Surface};
 
-    let fragment_shader_src = r#"
-        #version 140
-        out vec4 color;
-        void main() {
-            color = vec4(1.0, 0.0, 0.0, 1.0);
-        }
-    "#;
+    let lt_vertex = Vertex { position: [-0.5, 0.25] };
+    let rt_vertex = Vertex { position: [0.5, 0.25] };
+    let rb_vertex = Vertex { position: [0.5, -0.25] };
+    let lb_vertex = Vertex { position: [-0.5, -0.25] };
+    let shape = vec![lt_vertex, rt_vertex, lb_vertex, rb_vertex];
+    let patch_renderer = PatchRenderer::new();
 
-    let program = glium::Program::from_source(&display, vertex_shader_src, fragment_shader_src, None).unwrap();
-
+    let vertex_buffer = glium::VertexBuffer::new(&patch_renderer.display, &shape).unwrap();
+    let indices = glium::index::NoIndices(glium::index::PrimitiveType::TriangleStrip);
     loop {
-        let mut target = display.draw();
+        let mut target = patch_renderer.display.draw();
         target.clear_color(0.70, 0.80, 0.90, 1.0);
-        target.draw(&vertex_buffer, &indices, &program, &glium::uniforms::EmptyUniforms,
+        target.draw(&vertex_buffer, &indices, &patch_renderer.program, &glium::uniforms::EmptyUniforms,
                     &Default::default()).unwrap();
         target.finish().unwrap();
 
-        for ev in display.poll_events() {
+        for ev in patch_renderer.display.poll_events() {
             match ev {
                 glium::glutin::Event::Closed => return,
                 _ => ()
             }
         }
     }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        assert_eq!(4, 2 + 2);
+    }
+
+    #[test]
+    fn patch_renders() {}
 }
