8c14459e58000180e0d3cdf2682049aad6c6a12b
Jeffrey Yu <wehjin@gmail.com>
2016-11-05 11:59:19 -0700

    Fix formatting

--- patchgl/src/main.rs
+++ patchgl/src/main.rs
@@ -1,24 +1,23 @@
-#[macro_use]
-extern crate glium;
+#[macro_use] extern crate glium;
 
 fn main() {
-  use glium::{DisplayBuild, Surface};
-  let display = glium::glutin::WindowBuilder::new().build_glium().unwrap();
+    use glium::{DisplayBuild, Surface};
+    let display = glium::glutin::WindowBuilder::new().build_glium().unwrap();
 
-	#[derive(Copy, Clone)]
-	struct Vertex {
-		position: [f32;2],
-	}
-	implement_vertex!(Vertex, position);
+    #[derive(Copy, Clone)]
+    struct Vertex {
+        position: [f32; 2],
+    }
+    implement_vertex!(Vertex, position);
 
-	let vertex1 = Vertex {position: [-0.5, -0.5]};
-	let vertex2 = Vertex {position: [ 0.0,  0.5]};
-	let vertex3 = Vertex {position: [ 0.5, -0.25]};
-	let shape = vec![vertex1, vertex2, vertex3];
+    let vertex1 = Vertex { position: [-0.5, -0.5] };
+    let vertex2 = Vertex { position: [0.0, 0.5] };
+    let vertex3 = Vertex { position: [0.5, -0.25] };
+    let shape = vec![vertex1, vertex2, vertex3];
 
-	let vertex_buffer = glium::VertexBuffer::new(&display, &shape).unwrap();
-	let indices = glium::index::NoIndices(glium::index::PrimitiveType::TrianglesList);
-  let vertex_shader_src = r#"
+    let vertex_buffer = glium::VertexBuffer::new(&display, &shape).unwrap();
+    let indices = glium::index::NoIndices(glium::index::PrimitiveType::TrianglesList);
+    let vertex_shader_src = r#"
 		#version 140
 		in vec2 position;
 		void main() {
@@ -26,28 +25,28 @@ fn main() {
 		}
 	"#;
 
-	let fragment_shader_src = r#"
+    let fragment_shader_src = r#"
         #version 140
         out vec4 color;
         void main() {
             color = vec4(1.0, 0.0, 0.0, 1.0);
         }
-  "#;
+    "#;
 
-  let program = glium::Program::from_source(&display, vertex_shader_src, fragment_shader_src, None).unwrap();
+    let program = glium::Program::from_source(&display, vertex_shader_src, fragment_shader_src, None).unwrap();
 
-	loop {
-		let mut target = display.draw();
-		target.clear_color(0.70, 0.80, 0.90, 1.0);
-    target.draw(&vertex_buffer, &indices, &program, &glium::uniforms::EmptyUniforms,
+    loop {
+        let mut target = display.draw();
+        target.clear_color(0.70, 0.80, 0.90, 1.0);
+        target.draw(&vertex_buffer, &indices, &program, &glium::uniforms::EmptyUniforms,
                     &Default::default()).unwrap();
-		target.finish().unwrap();
+        target.finish().unwrap();
 
-		for ev in display.poll_events() {
-			match ev {
-				glium::glutin::Event::Closed => return,
-				_ => ()
-			}	
-	  }
-	}
+        for ev in display.poll_events() {
+            match ev {
+                glium::glutin::Event::Closed => return,
+                _ => ()
+            }
+        }
+    }
 }
