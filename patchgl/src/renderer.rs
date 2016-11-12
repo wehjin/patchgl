70274630f7ef38a0354bd221bea7464cdfe47161
Jeffrey Yu <wehjin@gmail.com>
2016-11-12 14:13:07 -0800

    Move shaders into files

--- patchgl/src/renderer.rs
+++ patchgl/src/renderer.rs
@@ -13,21 +13,8 @@ impl PatchRenderer {
         use glium::{DisplayBuild};
         let display = glium::glutin::WindowBuilder::new().build_glium().unwrap();
 
-        let vertex_shader_src = r#"
-            #version 140
-            in vec2 position;
-            void main() {
-                gl_Position = vec4(position, 0.0, 1.0);
-            }
-        "#;
-
-        let fragment_shader_src = r#"
-            #version 140
-            out vec4 color;
-            void main() {
-                color = vec4(1.0, 0.0, 0.0, 1.0);
-            }
-        "#;
+        let vertex_shader_src = include_str!("shaders/patch_vertex_shader.glsl");
+        let fragment_shader_src = include_str!("shaders/patch_fragment_shader.glsl");
 
         let program = glium::Program::from_source(&display, vertex_shader_src, fragment_shader_src, None).unwrap();
         let vertex_buffer = glium::VertexBuffer::new(&display, &patch.as_trianglelist()).unwrap();
