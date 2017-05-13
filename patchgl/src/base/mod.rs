85a9eecd951105083be8ff5ae31a4b820f42c807
Jeffrey Yu <wehjin@gmail.com>
2017-05-13 04:47:21 -0700

    Delete Glyff, Sigil, Shape

--- patchgl/src/base/mod.rs
+++ patchgl/src/base/mod.rs
@@ -1,24 +1,14 @@
 mod sdf;
 mod rectangle;
-mod glyff;
-mod glyff_center;
 mod color;
-mod shape;
 mod typeface;
 mod alignment;
-mod sigil;
-mod sigil_list;
 
 pub use self::sdf::*;
 pub use self::rectangle::*;
-pub use self::glyff::*;
-pub use self::glyff_center::*;
 pub use self::color::Color;
-pub use self::shape::Shape;
 pub use self::typeface::Typeface;
 pub use self::alignment::Alignment;
-pub use self::sigil::Sigil;
-pub use self::sigil_list::{SigilList, BasicSigilList};
 
 #[cfg(test)]
 mod tests {}
--- patchgl/src/base/shape.rs
+++
@@ -1,7 +0,0 @@
-use super::Typeface;
-use super::Alignment;
-
-pub enum Shape {
-    Rectangle,
-    Word(String, Typeface, Alignment)
-}
