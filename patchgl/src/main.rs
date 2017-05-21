956992775df901568d883606e03bac94b828b29e
Jeffrey Yu <wehjin@gmail.com>
2017-05-21 06:33:33 -0700

    Multiple quips

--- patchgl/src/main.rs
+++ patchgl/src/main.rs
@@ -16,21 +16,28 @@ fn main() {
             sigil: Sigil::FilledRectangle(Color::blue()),
             width: 300.0,
             height: 320.0,
-            approach: 0.0,
+            approach: 0.00,
             anchor: Anchor::top_left()
         });
         screen.add_block(2, Block {
             sigil: Sigil::FilledRectangle(Color::grey()),
             width: 80.0,
             height: 480.0,
-            approach: 0.01,
+            approach: 0.02,
             anchor: Anchor::top_left()
         });
         screen.add_block(3, Block {
             sigil: Sigil::Paragraph { line_height: 24.0, text: "I for one welcome our new robot overlords".to_string() },
             width: 320.0,
             height: 480.0,
-            approach: 0.02,
+            approach: 0.01,
+            anchor: Anchor::top_left()
+        });
+        screen.add_block(4, Block {
+            sigil: Sigil::Paragraph { line_height: 16.0, text: "all your base is belong to us".to_string() },
+            width: 80.0,
+            height: 480.0,
+            approach: 0.03,
             anchor: Anchor::top_left()
         });
         thread::sleep(Duration::from_secs(40));
