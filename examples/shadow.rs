ea4700a633a1aa3edf3309aab8ae3ded2daf15e9
Jeffrey Yu <92849+wehjin@users.noreply.github.com>
2018-04-04 12:46:49 -0700

    Add left and right panels to shadow volume

--- examples/shadow.rs
+++ examples/shadow.rs
@@ -13,9 +13,9 @@ fn main() {
     window::start(320, 400, |window| {
         let panel_padding = Padding::Uniform(Length::Spacing / 2);
         let panels = vec![
-            Flood::Color(Color::grey()) + Padding::Behind(Length::Spacing / 4) + panel_padding.clone(),
-            Flood::Color(Color::grey()) + Padding::Behind(Length::Spacing / 2) + panel_padding.clone(),
-            Flood::Color(Color::grey()) + Padding::Behind(Length::Spacing) + panel_padding.clone(),
+            Flood::Color(Color::red()) + Padding::Behind(Length::Spacing / 4) + panel_padding.clone(),
+            Flood::Color(Color::green()) + Padding::Behind(Length::Spacing / 2) + panel_padding.clone(),
+            Flood::Color(Color::blue()) + Padding::Behind(Length::Spacing) + panel_padding.clone(),
         ];
         let bar = patchgl::flood::bar(panels);
         let flood = bar
--- src/rendering/shadow_renderer.rs
+++ src/rendering/shadow_renderer.rs
@@ -22,7 +22,7 @@ impl ShadowRenderer {
             display,
             SHADOW_TRIANGLELIST_VERTEX_COUNT).unwrap();
         let indices = glium::index::NoIndices(glium::index::PrimitiveType::TrianglesList);
-        let color = Color::new(0.5, 1.0, 0.0, 0.0).to_gl();
+        let color = Color::custom_white(0.4).to_gl();
         let draw_parameters = glium::DrawParameters {
             depth: glium::Depth { test: glium::DepthTest::IfLess, write: true, ..Default::default() },
             blend: glium::Blend::alpha_blending(),
@@ -46,32 +46,93 @@ impl ShadowRenderer {
     }
 }
 
-const SHADOW_TRIANGLELIST_VERTEX_COUNT: usize = 6;
+const PANEL_VERTEX_COUNT: usize = 6;
+const PANEL_COUNT: usize = 3;
+const SHADOW_TRIANGLELIST_VERTEX_COUNT: usize = PANEL_COUNT * PANEL_VERTEX_COUNT;
 
 impl Patch {
-    fn shadow_trianglelist(&self, (screen_width, screen_height): (f32, f32)) -> Vec<Vertex> {
-        let screen_half_height = screen_height / 2.0;
-        let light_x = screen_width / 2.0;
-        let light_y = -2.0 * screen_half_height;
-        let light_z = 3.0 * screen_half_height;
-
-        let (left, right, bottom, _, far, _) = self.cage.limits();
-        let shadow_factor = light_z / (light_z - far);
-        let distance_bottom_from_light = bottom - light_y;
-        let shadow_bottom = light_y + distance_bottom_from_light * shadow_factor;
-        let distance_left_from_light = left - light_x;
-        let shadow_left = light_x + distance_left_from_light * shadow_factor;
-        let distance_right_from_light = right - light_x;
-        let shadow_right = light_x + distance_right_from_light * shadow_factor;
-
-        let panel_top = bottom;
-        let panel_bottom = 0.0;
-        let panel_bottom_left = shadow_left;
-        let panel_bottom_right = shadow_right;
-        let lt_vertex = Vertex { position: [left, panel_top, far] };
-        let rt_vertex = Vertex { position: [right, panel_top, far] };
-        let rb_vertex = Vertex { position: [panel_bottom_right, shadow_bottom, panel_bottom] };
-        let lb_vertex = Vertex { position: [panel_bottom_left, shadow_bottom, panel_bottom] };
-        vec![lt_vertex, rt_vertex, lb_vertex, lb_vertex, rt_vertex, rb_vertex]
+    fn shadow_trianglelist(&self, screen_dim: (f32, f32)) -> Vec<Vertex> {
+        let surface = self.cage.limits();
+        let shadow = shadow_dim(screen_dim, surface);
+        let (lt_bottom, rt_bottom, rb_bottom, lb_bottom) = bottom_panel(surface, shadow);
+        let (lt_right, rt_right, rb_right, lb_right) = right_panel(surface, shadow);
+        let (lt_left, rt_left, rb_left, lb_left) = left_panel(surface, shadow);
+        vec![lt_bottom, rt_bottom, lb_bottom, lb_bottom, rt_bottom, rb_bottom,
+             lt_right, rt_right, lb_right, lb_right, rt_right, rb_right,
+             lt_left, rt_left, lb_left, lb_left, rt_left, rb_left,
+        ]
     }
+}
+
+fn left_panel(surface: (f32, f32, f32, f32, f32, f32), shadow: (f32, f32, f32, f32)) -> (Vertex, Vertex, Vertex, Vertex) {
+    let (surface_left, _surface_right, surface_bottom, _surface_top, surface_far, _) = surface;
+    let (shadow_left, shadow_top, _shadow_right, shadow_bottom) = shadow;
+
+    let panel_top_x = if shadow_left > surface_left {
+        shadow_left
+    } else {
+        surface_left
+    };
+    let lt_vertex = Vertex { position: [panel_top_x, shadow_top, surface_far] };
+    let rt_vertex = Vertex { position: [panel_top_x, surface_bottom, surface_far] };
+    let rb_vertex = Vertex { position: [shadow_left, shadow_bottom, 0.0] };
+    let lb_vertex = Vertex { position: [shadow_left, shadow_top, 0.0] };
+    (lt_vertex, rt_vertex, rb_vertex, lb_vertex)
+}
+
+fn right_panel(surface: (f32, f32, f32, f32, f32, f32), shadow: (f32, f32, f32, f32)) -> (Vertex, Vertex, Vertex, Vertex) {
+    let (_surface_left, surface_right, surface_bottom, _surface_top, surface_far, _) = surface;
+    let (_shadow_left, shadow_top, shadow_right, shadow_bottom) = shadow;
+
+    let panel_top_x = if shadow_right < surface_right {
+        shadow_right
+    } else {
+        surface_right
+    };
+    let lt_vertex = Vertex { position: [panel_top_x, surface_bottom, surface_far] };
+    let rt_vertex = Vertex { position: [panel_top_x, shadow_top, surface_far] };
+    let rb_vertex = Vertex { position: [shadow_right, shadow_top, 0.0] };
+    let lb_vertex = Vertex { position: [shadow_right, shadow_bottom, 0.0] };
+    (lt_vertex, rt_vertex, rb_vertex, lb_vertex)
+}
+
+fn bottom_panel(surface: (f32, f32, f32, f32, f32, f32), shadow: (f32, f32, f32, f32)) -> (Vertex, Vertex, Vertex, Vertex) {
+    let (surface_left, surface_right, surface_bottom, _surface_top, surface_far, _) = surface;
+    let (shadow_left, _shadow_top, shadow_right, shadow_bottom) = shadow;
+
+    let panel_top_left = if shadow_left > surface_left {
+        shadow_left
+    } else {
+        surface_left
+    };
+    let panel_top_right = if shadow_right < surface_right {
+        shadow_right
+    } else {
+        surface_right
+    };
+    let lt_vertex = Vertex { position: [panel_top_left, surface_bottom, surface_far] };
+    let rt_vertex = Vertex { position: [panel_top_right, surface_bottom, surface_far] };
+    let rb_vertex = Vertex { position: [shadow_right, shadow_bottom, 0.0] };
+    let lb_vertex = Vertex { position: [shadow_left, shadow_bottom, 0.0] };
+    (lt_vertex, rt_vertex, rb_vertex, lb_vertex)
+}
+
+fn shadow_dim((screen_width, screen_height): (f32, f32),
+              (left, right, bottom, top, far, _): (f32, f32, f32, f32, f32, f32)) -> (f32, f32, f32, f32) {
+    let screen_half_height = screen_height / 2.0;
+    let light_x = screen_width / 2.0;
+    let light_y = -0.25 * screen_half_height;
+    let light_z = 2.0 * screen_half_height;
+    let distance_top_from_light = top - light_y;
+    let distance_bottom_from_light = bottom - light_y;
+    let distance_left_from_light = left - light_x;
+    let distance_right_from_light = right - light_x;
+
+    let shadow_factor = light_z / (light_z - far);
+
+    let shadow_top = light_y + distance_top_from_light * shadow_factor;
+    let shadow_bottom = light_y + distance_bottom_from_light * shadow_factor;
+    let shadow_left = light_x + distance_left_from_light * shadow_factor;
+    let shadow_right = light_x + distance_right_from_light * shadow_factor;
+    (shadow_left, shadow_top, shadow_right, shadow_bottom)
 }
