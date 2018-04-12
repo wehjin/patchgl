8024ea28366c2842f29cd30635674d9e3aee9d71
Jeffrey Yu <92849+wehjin@users.noreply.github.com>
2018-04-11 17:06:44 -0700

    Render text at different sizes

--- examples/type_sizes.rs
+++ examples/type_sizes.rs
@@ -9,7 +9,7 @@ use patchgl::traits::*;
 
 fn main() {
     use patchgl::app;
-    app::run(480, 800, "Typography", AppMdl::default());
+    app::run(480, 800, "Type sizes", AppMdl::default());
 }
 
 #[derive(Clone, PartialEq, Debug)]
@@ -31,6 +31,31 @@ impl Update<AppMsg> for AppMdl {
 impl Draw<AppMsg> for AppMdl {
     fn draw(&self) -> Flood<AppMsg> {
         let palette = patchgl::material::Palette::default();
-        Flood::Color(palette.light_background)
+        let text_color = palette.dark_background_text_primary;
+        Flood::Color(palette.transparent)
+            + (Position::Top(Length::Spacing), Flood::Color(palette.transparent))
+            + (Position::Top(Length::Pixels(14.1)), Flood::Text("Medium (All Caps) 14".to_uppercase(), text_color, Placement::Start))
+            + (Position::Top(Length::Spacing), Flood::Color(palette.transparent))
+            + (Position::Top(Length::Pixels(12.0)), Flood::Text("Regular 12".into(), text_color, Placement::Start))
+            + (Position::Top(Length::Spacing), Flood::Color(palette.transparent))
+            + (Position::Top(Length::Pixels(14.9)), Flood::Text("Regular 14".into(), text_color, Placement::Start))
+            + (Position::Top(Length::Spacing), Flood::Color(palette.transparent))
+            + (Position::Top(Length::Pixels(14.8)), Flood::Text("Medium 14".into(), text_color, Placement::Start))
+            + (Position::Top(Length::Spacing), Flood::Color(palette.transparent))
+            + (Position::Top(Length::Pixels(16.7)), Flood::Text("Regular 16".into(), text_color, Placement::Start))
+            + (Position::Top(Length::Spacing), Flood::Color(palette.transparent))
+            + (Position::Top(Length::Pixels(20.6)), Flood::Text("Medium 20".into(), text_color, Placement::Start))
+            + (Position::Top(Length::Spacing), Flood::Color(palette.transparent))
+            + (Position::Top(Length::Pixels(24.5)), Flood::Text("Regular 24".into(), text_color, Placement::Start))
+            + (Position::Top(Length::Spacing), Flood::Color(palette.transparent))
+            + (Position::Top(Length::Pixels(34.4)), Flood::Text("Regular 34".into(), text_color, Placement::Start))
+            + (Position::Top(Length::Spacing), Flood::Color(palette.transparent))
+            + (Position::Top(Length::Pixels(45.3)), Flood::Text("Regular 45".into(), text_color, Placement::Start))
+            + (Position::Top(Length::Spacing), Flood::Color(palette.transparent))
+            + (Position::Top(Length::Pixels(56.2)), Flood::Text("Regular 56".into(), text_color, Placement::Start))
+            + (Position::Top(Length::Spacing), Flood::Color(palette.transparent))
+            + (Position::Top(Length::Pixels(112.1)), Flood::Text("Light 112".into(), text_color, Placement::Start))
+            + Padding::Uniform(Length::Spacing)
+            + Flood::Color(palette.dark_background)
     }
 }
